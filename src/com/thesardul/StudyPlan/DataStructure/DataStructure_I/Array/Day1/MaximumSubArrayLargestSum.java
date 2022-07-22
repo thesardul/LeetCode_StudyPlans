86893b4b58bc4b0f5a2eb6311298ec4a94a12f4b
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-07-23 02:25:09 +0530

    Data Structure Day 1 Question 2
    Maximum Subarray: Largest sum contiguous subarray

---
+++ src/com/thesardul/StudyPlan/DataStructure/DataStructure_I/Array/Day1/MaximumSubArrayLargestSum.java
@@ -0,0 +1,20 @@
+package com.thesardul.StudyPlan.DataStructure.DataStructure_I.Array.Day1;
+
+public class MaximumSubArrayLargestSum {
+    public static void main(String[] args) {
+        int[] a = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(a));
+    }
+
+    private static int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE, maxSumHere = 0;
+        for(int i = 0; i < nums.length; i++){
+            maxSumHere += nums[i];
+            if(maxSum < maxSumHere)
+                maxSum = maxSumHere;
+            if(maxSumHere < 0)
+                maxSumHere = 0;
+        }
+        return maxSum;
+    }
+}
